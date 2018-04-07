5c39b23f1caa98835aa7e3de9278a1ba5f812c3e
thilinasandaru1 <thilinasandaru1@gmail.com>
2018-04-08 00:25:38 +0530

    fixed browsersync live server

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
 var browserSync = require('browser-sync').create();
 
-// Compile LESS
+///////// Compile LESS
 gulp.task('less', function () {
   return gulp.src('./less/*.less')
     .pipe(less({
@@ -13,15 +13,25 @@ gulp.task('less', function () {
     .pipe(browserSync.stream());
 });
 
+// create a task that ensures the `less` task is complete before
+// reloading browsers
+gulp.task('less-watch', ['less'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+
+
+
 // Live Server with Browser Sync
 // Static Server + watching less/html files
-gulp.task('serve', ['less'], function() {
+gulp.task('serve', ['less-watch'], function() {
 
     browserSync.init({
         server: "./"
     });
 
-    gulp.watch("less/*.less", ['less']);
+    gulp.watch("less/*.less", ['less-watch']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
