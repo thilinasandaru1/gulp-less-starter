664ff034731d88419496bb1318bd6206856a76bf
thilinasandaru1 <thilinasandaru1@gmail.com>
2018-04-08 00:09:12 +0530

    added live server function via browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['less']);
-
- 
+// Compile LESS
 gulp.task('less', function () {
   return gulp.src('./less/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
+
+// Live Server with Browser Sync
+// Static Server + watching less/html files
+gulp.task('serve', ['less'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("less/*.less", ['less']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Default task
+gulp.task('default', ['serve']);
