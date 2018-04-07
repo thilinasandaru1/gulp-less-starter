6e6d7fac3db52c99c711333e61e6b0e31f981f78
thilinasandaru1 <thilinasandaru1@gmail.com>
2018-04-07 23:30:17 +0530

    added gulp config for gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['less']);
+
+ 
+gulp.task('less', function () {
+  return gulp.src('./less/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
 });
-
-
